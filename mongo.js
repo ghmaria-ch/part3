3753070130d333695f25313be5f88126b4a9a02a
maria christo <maria.phone.2020@gmail.com>
2025-02-25 03:02:45 +0530

    changed mongo.js

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://kadalthavala:${password}@cluster0.djilz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://mariaphone2020:${password}@cluster0.djilz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
